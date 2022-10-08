09a87e02b736934d16fb22b6d492e8214688f322
Mitzi17 <m_estrada17@live.com>
2022-10-07 17:37:24 -0700

    add div for main todo list with an h1 for title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      REACT
+      
+      <div className='main-todo-list'>
+        <h1>My To-do's:</h1>
+      </div>
+      
     </div>
+
   );
 }
 
