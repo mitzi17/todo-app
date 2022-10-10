b6d73e79cd7e03db82c3717f5aa35be5d26ba233
Mitzi17 <m_estrada17@live.com>
2022-10-09 21:18:23 -0700

    use react icon in div

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../stylesheets/Todo.css'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
-function Todo({ text }) {
+function Todo({ text, complete }) {
     return(
-        <div className='todo-container'>
+        <div className={complete ? 'todo-container complete' : 'todo-container'}>
             <div className='todo-text'>
                 {text}
             </div>
-            <div className='todo-icon'>
-                Delete
+            <div className='todo-container-icons'>
+                <AiOutlineCloseCircle className='todo-icon'/>
             </div>
         </div>
     )
