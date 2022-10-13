234383fb888e9a5f40cb1b1860faaebed28c705c
Mitzi17 <m_estrada17@live.com>
2022-10-12 23:18:25 -0700

    define completeTodo and deleTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,19 +8,41 @@ function TodoList() {
 const [ todos, setTodos ] = useState([])
 
 const addTodo = todo => {
-    console.log("Todo added")
-    console.log(todo)
+    if (todo.text.trim()) {
+        todo.text = todo.text.trim()
+        const updatedTodos = [ todo, ...todos ]
+        setTodos(updatedTodos)
+    }
+}
+
+const deleteTodo = id => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos(updatedTodos)
+}
+
+const completeTodo = id => {
+    const updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.complete = !todo.complete
+        }
+        return todo
+    })
+    setTodos(updatedTodos)
 }
 
     return (
         <>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
             <div className='todo-list-container'>
                 {
                     todos.map((todo) =>
                         <Todo
+                        key={todo.id}
+                        id={todo.id}
                         text={todo.text}
-                        complete={todo.complete} />
+                        complete={todo.complete}
+                        deleteTodo={deleteTodo}
+                        completeTodo={completeTodo} />
                     )
                 }
             </div>
