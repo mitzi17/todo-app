cec22e982bfd7af5be8e1094eaa152304dc454be
Mitzi17 <m_estrada17@live.com>
2022-10-12 17:24:19 -0700

    define handleChange and handleSubmit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../stylesheets/TodoForm.css'
 
 function TodoForm(props) {
+
+    const [ input, setInput ] = useState('')
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+        
+        const newTodo = {
+            id: '34545',
+            text: input,
+            complete: false
+        }
+    }
+
     return(
-        <form className='todo-form'>
+        <form
+            className='todo-form'
+            onSubmit={handleSubmit}>
             <input 
-            className='todo-input'
-            type='text'
-            placeholder='Type a to-do'
-            name='text'
+                className='todo-input'
+                type='text'
+                placeholder='Type a to-do'
+                name='text'
+                onChange={handleChange}
             />
             <button className='todo-button'>
                 Add to-do
